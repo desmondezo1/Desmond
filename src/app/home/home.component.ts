0b170348bcae1954d80a6aa7fe1d86e4aaea4436
Desmond <desezo@gmail.com>
2020-11-26 23:09:21 +0100

    done animating text on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+//import * as anime from 'animejs';
+import anime from 'node_modules/animejs/lib/anime.es.js';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
+anime: any
 
-  constructor() { }
 
-  ngOnInit(): void {
+constructor() {
+}
+
+   ngAfterViewInit(): void {
+    let tl = anime.timeline({
+        easing: 'easeOutExpo',
+        duration: 1000
+      });
+
+      tl.add({
+        targets: [".hi",".desmond",".creative",".designer"],
+     opacity: 0
+      }).add({
+     targets: ".hi",
+     opacity: 1
+    }).add({
+      targets: ".desmond",
+      opacity: 1
+    }).add({
+      delay: 1500,
+      targets:".creative",
+      opacity: 1
+    }).add({
+      targets:".designer",
+      opacity: 1
+    })
+
+   }
   }
 
-}
+
+  // ngOnInit(): void {
+    
+    
+
+ 
+
+  // }
+
+
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+//import anime from 'node_modules/animejs/lib/anime.es.js';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
+  ngAfterViewInit(): void{
+
+  //  let tl = anime.timeline({
+  //       easing: 'easeOutExpo',
+  //       duration: 500,
+  //       delay: 3000
+  //     });
+
+  //     tl.ad({
+  //       targets: [".nav",".social-icons","bottom-nav"],
+  //       opacity: 0
+  //     }).ad({
+  //       targets: ".nav",
+  //       translateY: -400
+  //     }).add({
+  //       targets: ".social-icons",
+  //       translateX: -400
+  //     }).add({
+  //       targets: ".bottom-nav",
+  //       translateY: -400
+  //     })
+
+      
+
+
+  }
 }
