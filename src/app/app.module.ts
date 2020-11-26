0adfd5708c8346456a8f28175426910d36f61f88
Desmond <desezo@gmail.com>
2020-11-26 08:30:20 +0100

    home page done - no animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoadScreenComponent } from './load-screen/load-screen.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import {appRoutes} from '../routes'
+import { RouterModule } from '@angular/router';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadScreenComponent,
+    PortfolioComponent,
+    AboutmeComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+import { AboutmeComponent } from "./app/aboutme/aboutme.component";
+import { HomeComponent } from "./app/home/home.component";
+import { PortfolioComponent } from "./app/portfolio/portfolio.component";
+
+export const appRoutes:Routes = [
+    {path: '', component: HomeComponent },
+    {path: 'portfolio', component: PortfolioComponent},
+    {path: 'about', component: AboutmeComponent}
+]
